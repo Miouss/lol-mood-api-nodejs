24fb7d4e0253d3b61ba934a84463f0b4b99fa5ef
Miouss <samir.ghabi@gmail.com>
2023-09-06 22:55:45 +0200

    More stats handled

--- src/routes/champ/middlewares/getChampGames.ts
+++ src/routes/champ/middlewares/getChampGames.ts
@@ -14,128 +14,177 @@ export async function getChampGames(
     const { champName } = req.params;
 
     const games = await GameInfo.getByChamp(champName);
-    if (!games) throw new Error("No games found for this champion");
-
-    const completedItemsStats = getCompleteditemsList(games);
 
     const startItemsStats = getStartItemsStats(games);
 
-    res.json({ startItemsStats, completedItemsStats });
+    const completedItemsStats = getCompleteditemsList(games);
+
+    const mostPlayedStatsMods = getMostPlayedStatsMods(games);
+
+    res.json({ startItemsStats });
   } catch (err) {
     next(err);
   }
+}
+
+function getMostPlayedStatsMods(games: ParticipantMatchDataResponse[]) {
+  const statsModsStatsList: StatsModStats[][] = [[], [], []];
+  const defaultStats: StatsModStats = { statMod: 0, played: 0 };
+
+  games.forEach((game) => {
+    game.statsMods.forEach((statMod, i) => {
+      const statsModsStats = statsModsStatsList[i];
+
+      const index = statsModsStats.findIndex(
+        (currStatMod) => statMod === currStatMod.statMod
+      );
+
+      const hasFoundIndex = index >= 0;
+
+      if (!hasFoundIndex) {
+        statsModsStats.push({ ...defaultStats, statMod });
+      }
+
+      statsModsStats[hasFoundIndex ? index : statsModsStats.length - 1]
+        .played++;
+    });
+  });
+
+  statsModsStatsList.forEach((statsMods) => {
+    sortByMostPlayed(statsMods);
+  });
+
+  const mostPlayedStatsMods = statsModsStatsList.map(
+    (statsMods) => statsMods[0].statMod
+  );
+
+  return mostPlayedStatsMods;
 }
 
 function getStartItemsStats(games: ParticipantMatchDataResponse[]) {
   const startItemsStats: StartItemsStats[] = [];
   const totalPlayed = games.length;
 
+  const defaultItemStats: StartItemsStats = {
+    items: [],
+    played: 0,
+    wins: 0,
+    playrate: 0,
+    winrate: 0,
+  };
+
   games.forEach((game) => {
     const index = startItemsStats.findIndex((startItems) =>
       startItems.items.every((item, i) => item === game.startItems[i])
     );
 
-    const startItems: StartItemsStats =
-      startItemsStats[index] ??
-      ({
-        items: game.startItems,
-        played: 0,
-        wins: 0,
-      } as StartItemsStats);
-
-    startItems.played++;
-    startItems.wins += Number(game.win);
-
     const hasFoundIndex = index >= 0;
 
-    if (hasFoundIndex) startItemsStats[index] = startItems;
-    else startItemsStats.push(startItems);
+    if (!hasFoundIndex) {
+      startItemsStats.push({ ...defaultItemStats, items: game.startItems });
+    }
+
+    const stats =
+      startItemsStats[hasFoundIndex ? index : startItemsStats.length - 1];
+    stats.played++;
+    stats.wins += Number(game.win);
   });
 
-  startItemsStats.forEach((startItems) => {
-    startItems.playrate = twoDecimalsNumByPercent(
-      startItems.played / totalPlayed
-    );
-    startItems.winrate = twoDecimalsNumByPercent(
-      startItems.wins / startItems.played
-    );
+  startItemsStats.forEach((stats) => {
+    calculateWinAndPlayrate(stats, totalPlayed);
   });
 
   return startItemsStats;
 }
 
 function getCompleteditemsList(games: ParticipantMatchDataResponse[]) {
-  const completedItemsStatsList: CompletedItemsStatsList[] = [
-    { totalPlayed: 0 },
-    { totalPlayed: 0 },
-    { totalPlayed: 0 },
-    { totalPlayed: 0 },
-    { totalPlayed: 0 },
-  ];
+  const NUM_COMPLETED_ITEMS = 5;
+  const defaultItemsStats = { items: [], totalPlayed: 0 };
+
+  const completedItemsStatsList: CompletedItemsStatsList[] =
+    createArrayOfSameObj(defaultItemsStats, NUM_COMPLETED_ITEMS);
+
+  const defaultItem: CompletedItemsStats = {
+    itemId: 0,
+    played: 0,
+    wins: 0,
+    playrate: 0,
+    winrate: 0,
+  };
 
   games.forEach((game) => {
     game.completedItems.every((itemId, i) => {
-      console.log(itemId, i);
       if (!itemId) return false;
 
-      const itemStats: CompletedItemsStats =
-        completedItemsStatsList[i][itemId] ??
-        ({
-          itemId,
-          played: 0,
-          wins: 0,
-          playrate: 0,
-          winrate: 0,
-        } as CompletedItemsStats);
+      const items = completedItemsStatsList[i].items;
 
-      itemStats.played++;
-      itemStats.wins += Number(game.win);
-      itemStats.winrate = twoDecimalsNumByPercent(
-        itemStats.wins / itemStats.played
-      );
+      const index = items.findIndex((currItem) => itemId === currItem.itemId);
+      const hasFoundIndex = index >= 0;
+
+      if (!hasFoundIndex) {
+        items.push({ ...defaultItem, itemId });
+      }
+
+      const item = items[hasFoundIndex ? index : items.length - 1];
+
+      item.played++;
+      item.wins += Number(game.win);
 
       completedItemsStatsList[i].totalPlayed++;
-
-      completedItemsStatsList[i][itemId] = itemStats;
 
       return true;
     });
   });
 
-  completedItemsStatsList.forEach((completedItemsStats) => {
-    Object.values(completedItemsStats).forEach(
-      (itemStats: CompletedItemsStats | number) => {
-        const isTotalPlayedProp = typeof itemStats === "number";
+  completedItemsStatsList.forEach((itemsStats) => {
+    itemsStats.items.forEach((stats: CompletedItemsStats | number) => {
+      const isTotalPlayedProp = typeof stats === "number";
 
-        if (isTotalPlayedProp) return;
-
-        itemStats.playrate = twoDecimalsNumByPercent(
-          itemStats.played / completedItemsStats.totalPlayed
-        );
-      }
-    );
+      if (!isTotalPlayedProp)
+        calculateWinAndPlayrate(stats, itemsStats.totalPlayed);
+    });
   });
 
   return completedItemsStatsList;
 }
 
+function createArrayOfSameObj<T>(obj: T, length: number) {
+  return Array.from({ length }).map((_) => JSON.parse(JSON.stringify(obj)));
+}
+
+function calculateWinAndPlayrate<T extends Stats>(
+  stats: T,
+  totalPlayed: number
+) {
+  stats.playrate = twoDecimalsNumByPercent(stats.played / totalPlayed);
+  stats.winrate = twoDecimalsNumByPercent(stats.wins / stats.played);
+}
+
+function sortByMostPlayed<T extends { played: number }>(statsList: T[]) {
+  statsList.sort((a, b) => b.played - a.played);
+}
+
+interface StatsModStats {
+  statMod: number;
+  played: number;
+}
+
 interface CompletedItemsStatsList {
-  [key: number]: CompletedItemsStats;
+  items: CompletedItemsStats[];
   totalPlayed: number;
 }
 
-interface CompletedItemsStats {
+interface CompletedItemsStats extends Stats {
   itemId: number;
-  played: number;
-  wins: number;
-  playrate: number;
-  winrate: number;
 }
 
-interface StartItemsStats {
+interface StartItemsStats extends Stats {
   items: number[];
+}
+
+interface Stats {
   played: number;
   wins: number;
-  winrate: number;
   playrate: number;
+  winrate: number;
 }
