737b9b4ea793858ff6a386ac1ef21df568de0b7d
Miouss <samir.ghabi@gmail.com>
2023-09-27 23:30:36 +0200

    Removed last itemId from DB because it's a ward

--- src/database/database.sql
+++ src/database/database.sql
@@ -1,9 +1,6 @@
 DROP DATABASE IF EXISTS lol_mood_db;
-
 CREATE DATABASE lol_mood_db;
 use lol_mood_db;
-
-
 CREATE TABLE account (
   id int AUTO_INCREMENT,
   puuid varchar(96) UNIQUE NOT NULL,
@@ -17,26 +14,20 @@ CREATE TABLE account (
   wins int NOT NULL DEFAULT 0,
   PRIMARY KEY (id)
 );
-
 CREATE TABLE game (
   id int AUTO_INCREMENT,
   identifier varchar(32) UNIQUE NOT NULL,
-
   PRIMARY KEY (id)
 );
-
 CREATE TABLE champ (
   id int AUTO_INCREMENT,
   name varchar(64) UNIQUE NOT NULL,
   PRIMARY KEY (id)
 );
-
 CREATE TABLE game_info (
   gameId int NOT NULL,
   accountId int NOT NULL,
   champId int NOT NULL,
-
-
   win boolean NOT NULL,
   kills int,
   deaths int,
@@ -44,51 +35,38 @@ CREATE TABLE game_info (
   skillsOrder VARCHAR(18),
   evolvesOrder VARCHAR(4),
   lane VARCHAR(8),
-
-
   primaryStyleId int NOT NULL,
   subStyleId int NOT NULL,
   perkId int NOT NULL,
-
   runeId0 int NOT NULL,
   runeId1 int NOT NULL,
   runeId2 int NOT NULL,
   runeId3 int NOT NULL,
   runeId4 int NOT NULL,
-
   statsModId0 int NOT NULL,
   statsModId1 int NOT NULL,
   statsModId2 int NOT NULL,
-
   summonerId0 int NOT NULL,
   summonerId1 int NOT NULL,
-
-
   itemId0 int,
   itemId1 int,
   itemId2 int,
   itemId3 int,
   itemId4 int,
   itemId5 int,
-  itemId6 int,
-
   startItemId0 int,
   startItemId1 int,
   startItemId2 int,
   startItemId3 int,
   startItemId4 int,
   startItemId5 int,
-
   completedItemId0 int,
   completedItemId1 int,
   completedItemId2 int,
   completedItemId3 int,
   completedItemId4 int,
   completedItemId5 int,
-
-
   PRIMARY KEY (gameId, accountId),
-
   FOREIGN KEY(gameId) REFERENCES game(id),
   FOREIGN KEY(accountId) REFERENCES account(id),
   FOREIGN KEY(champId) REFERENCES champ(id)
